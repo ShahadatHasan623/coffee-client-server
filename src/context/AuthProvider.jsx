7c39696ab16253baf48b0be7ef7d2745cd035ec0
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-16 18:09:51 +0600

    added onauth

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/firebase.config';
 
 const AuthProvider = ({children}) => {
-
+     const [user,setUser]=useState(null)
     // signUp 
     const signUp =(email,password)=>{
         return createUserWithEmailAndPassword(auth,email,password)
@@ -15,7 +15,18 @@ const AuthProvider = ({children}) => {
         return signInWithEmailAndPassword(auth,email,password)
     }
 
+    useEffect(()=>{
+        const unSubsCribe= onAuthStateChanged(auth,(currentUser)=>{
+            setUser(currentUser)
+        });
+        return()=>{
+            unSubsCribe()
+        }
+
+    },[])
+
     const UserInfo ={
+        user,
         signUp,
         signInUser
     }
