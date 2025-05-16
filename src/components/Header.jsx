fbdcfe5bd763dab901fcf034fabce3f7c184a5cb
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-16 20:52:57 +0600

    added dropdown nav btn nine

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="header-img navbar bg-base-100 shadow-sm lg:px-20 px-2">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div tabIndex={0} role="button" className="lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5 text-white"
